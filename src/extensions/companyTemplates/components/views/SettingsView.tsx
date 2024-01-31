611239486d1265e32272ac35bd70bcc0cfeaebde
Tobias Maestrini <tobias@bee365.ch>
2024-02-01 00:06:47 +0100

    add views for navigation component

--- src/extensions/companyTemplates/components/views/SettingsView.tsx
+++ src/extensions/companyTemplates/components/views/SettingsView.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import { useContext } from 'react';
 import { SPFxContext } from '../../contexts/SPFxContext';
-import { PrimaryButton } from '@fluentui/react';
+import { PrimaryButton, Stack } from '@fluentui/react';
 import { StandardView } from './StandardView';
+import { ISite, SitePicker } from "@pnp/spfx-controls-react/lib/SitePicker";
+import { ListPicker } from "@pnp/spfx-controls-react/lib/ListPicker";
+import { SPFx, spfi } from "@pnp/sp";
+import "@pnp/sp/appcatalog";
+import "@pnp/sp/webs";
+import { UserService } from '../../../../services/core/UserService';
 
 export interface ISettingsViewProps {
   onNavigationExit: (destination: React.ReactNode) => void;
@@ -10,16 +16,83 @@ export interface ISettingsViewProps {
 
 export const SettingsView: React.FunctionComponent<ISettingsViewProps> = (props: React.PropsWithChildren<ISettingsViewProps>) => {
   const context = useContext(SPFxContext).context;
-  console.log(context);
+  const [userToken, setUserToken] = React.useState(undefined);
+  const [settings, setSettings] = React.useState<{ site: string, list: string }>({ site: undefined, list: undefined });
+  const [inProgress, setInProgress] = React.useState(false);
+
+  async function storeListSettings(): Promise<void> {
+    const sp = spfi().using(SPFx(context));
+    const w = await sp.getTenantAppCatalogWeb();
+
+    // specify required key and value
+    await w.setStorageEntity("easyTemplatesListUrl", settings.site);
+    await w.setStorageEntity("easyTemplatesLibraryId", settings.list);
+
+    console.log('List selection saved');
+  }
+
+  React.useEffect(() => {
+    async function fetchListSettings() {
+      const sp = spfi().using(SPFx(context));
+      const web = await sp.getTenantAppCatalogWeb();
+      const listUrl = await web.getStorageEntity("easyTemplatesListUrl");
+      const listId = await web.getStorageEntity("easyTemplatesLibraryId");
+      setSettings({ site: listUrl.Value, list: listId.Value });
+    }
+
+    fetchListSettings().catch(error => console.log(error));
+  }, []);
+
+  React.useEffect(() => {
+    const userService = context.serviceScope.consume(UserService.serviceKey);
+    userService.getUserTokenDecoded()
+      .then((token) => { setUserToken(token) })
+      .catch((error) => console.log(error));
+  }, []);
+
 
   function save(): void {
-    props.onNavigationExit(<StandardView/>);
+    setInProgress(true);
+    storeListSettings()
+      .then(() => props.onNavigationExit(<StandardView />))
+      .catch(error => console.log(error));
   }
 
   return (
     <>
       <h1 key={'title'}>Settings</h1>
-      <PrimaryButton text="Save" onClick={save.bind(this)} allowDisabledFocus />
+      <Stack tokens={{
+        childrenGap: 10,
+        padding: 10,
+        maxWidth: '50%'
+      }}>
+
+        {userToken && <span>TenantId: {userToken.tid}</span>}
+
+        <SitePicker
+          context={context as any}
+          label={'Select sites'}
+          mode={'site'}
+          allowSearch={true}
+          multiSelect={false}
+          selectedSites={[{ url: settings.site }] as ISite[]}
+          onChange={(sites) => { setSettings({ ...settings, site: sites[0].url }) }}
+          placeholder={'Select sites'}
+          searchPlaceholder={'Filter sites'} />
+
+        <ListPicker context={context as any}
+          label="Select your list(s)"
+          placeHolder="Select your list(s)"
+          filter="BaseTemplate eq 101 and EntityTypeName ne 'FormServerTemplates' and EntityTypeName ne 'SiteAssets' and EntityTypeName ne 'Style_x0020_Library'"
+          includeHidden={false}
+          multiSelect={false}
+          disabled={settings.site === undefined}
+          webAbsoluteUrl={settings.site && settings.site}
+          selectedList={settings.list}
+          onSelectionChanged={value => setSettings({ ...settings, list: value as string })} />
+
+        <PrimaryButton disabled={inProgress} text="Save Settings" onClick={save.bind(this)} allowDisabledFocus />
+      </Stack>
     </>
   );
 };
--- src/extensions/companyTemplates/components/views/StandardView.tsx
+++ src/extensions/companyTemplates/components/views/StandardView.tsx
@@ -1,25 +1,120 @@
 import * as React from 'react';
 import { useContext } from 'react';
+import { groupBy } from '@microsoft/sp-lodash-subset';
 import { SPFxContext } from '../../contexts/SPFxContext';
-import { UserService } from '../../../../services/core/UserService';
+import { Icon, PrimaryButton, Spinner, SpinnerSize, Stack, StackItem, Text } from '@fluentui/react';
+import { FileIconType, getFileTypeIconProps } from '@fluentui/react-file-type-icons';
+import styles from '../CompanyTemplates.module.scss'
+import { ITreeItem, TreeView, TreeViewSelectionMode } from '@pnp/spfx-controls-react/lib/TreeView';
+import { SPFx, spfi } from '@pnp/sp';
+import "@pnp/sp/items/get-all";
+import "@pnp/sp/items";
+import { TemplateFile, useTemplateFiles } from '../../../../hooks/useTemplateFiles';
+
 
 export interface ITemplateViewProps { }
 
 export const StandardView: React.FunctionComponent<ITemplateViewProps> = (props: React.PropsWithChildren<ITemplateViewProps>) => {
   const context = useContext(SPFxContext).context;
-  const [userToken, setUserToken] = React.useState(undefined);
-  
+  // const [templateFiles, setFiles] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [selectedFiles, setSelectedFiles] = React.useState<ITreeItem[]>([]);
+  const { templateFiles, setParams } = useTemplateFiles({ context, listId: undefined, webUrl: undefined });
+
   React.useEffect(() => {
-    const userService = context.serviceScope.consume(UserService.serviceKey);
-    userService.getUserTokenDecoded()
-      .then(token => { setUserToken(token) })
-      .catch(er => console.log(er));
+    initSourceList().catch(error => console.log(error));
   }, []);
 
+  async function initSourceList(): Promise<void> {
+    setLoading(true);
+    const sp = spfi().using(SPFx(context));
+    const listUrl = await sp.web.getStorageEntity("easyTemplatesListUrl");
+    const listId = await sp.web.getStorageEntity("easyTemplatesLibraryId");
+    console.log(listId.Value);
+    console.log(listUrl.Value);
+
+    setParams({ context, listId: listId.Value, webUrl: listUrl.Value });
+    setLoading(false);
+  }
+
+  const onRenderItem = (treeItem: ITreeItem): JSX.Element => {
+    const { data } = treeItem;
+    if (data.type === 'Folder') {
+      return <div className={styles.treeNode}>
+        <Icon {...getFileTypeIconProps({ type: FileIconType.folder, size: 16, imageFileType: 'png' })} />
+        {treeItem.label}
+      </div>;
+    }
+
+    return <div className={styles.treeLeaf}>
+      <Stack horizontal horizontalAlign='space-between' style={{ width: '100%' }}>
+        <StackItem verticalFill={true}>
+          <Icon {...getFileTypeIconProps({ extension: data?.fileType, size: 16, imageFileType: 'png' })} style={{ verticalAlign: 'text-bottom' }} />
+          {treeItem.label}
+        </StackItem>
+        {/* <StackItem styles={{ root: { width: '120px', textAlign: 'right' } }}>
+          {new Date(data?.modified).toLocaleDateString()}
+        </StackItem> */}
+        <StackItem>
+          <Text variant='xSmall' className={styles.category}>Kategorie</Text>
+        </StackItem>
+      </Stack>
+    </div >;
+  }
+
+  const buildGroups = (items: any[], path = '', level = 1): any[] => {
+    if (!items?.length) return [];
+
+    const sorted = [...items].sort((a, b) => {
+      const aValue = a.filePath.toLowerCase();
+      const bValue = b.filePath.toLowerCase();
+      if (aValue > bValue) return 1;
+      if (bValue > aValue) return -1;
+      return 0;
+    });
+
+    const grouped = groupBy(sorted, (i) => {
+      if (i.pathSegments.length <= level) return '$this';
+      return i.pathSegments[level - 1];
+    });
+    return [
+      ...Object
+        .keys(grouped)
+        .filter((directory) => directory !== '$this')
+        .map((directory): any => ({
+          key: `${path}${directory}`,
+          label: directory,
+          subLabel: "Test",
+          data: { title: directory, type: 'Folder' },
+          selectable: false,
+          children: buildGroups(grouped[directory], `${path}${directory}/`, level + 1)
+        })),
+      ...grouped.$this?.filter(i => !i.title.includes('.DS'))
+        .map((i: TemplateFile) => ({
+          key: i.id,
+          label: i.fileLeafRef,
+          subLabel: i.filePath,
+          data: i,
+        })) ?? [],
+    ];
+  }
+
   return (
-    <>
+    <div>
       <h1 key={'title'}>Template View (Standard)</h1>
-      {userToken && <span>TenantId: {userToken.tid}</span>}
-    </>
+      {loading && <div><Spinner size={SpinnerSize.large} label='Loading Templates...' labelPosition='top' /></div>}
+      {!loading && <>
+        <PrimaryButton disabled={selectedFiles.length === 0} text={`${selectedFiles.length > 0 ? `${selectedFiles.length} ` : ''}Templates kopieren`} onClick={() => console.log('kopieren')} allowDisabledFocus />
+        <TreeView
+          items={buildGroups(templateFiles)}
+          defaultExpandedChildren={false}
+          showCheckboxes={true}
+          defaultExpanded={false}
+          selectionMode={TreeViewSelectionMode.Multiple}
+          onSelect={(items => { console.log(items); setSelectedFiles([...items]); })}
+          onRenderItem={(i) => onRenderItem(i)}
+        />
+      </>}
+    </div>
   );
 };
