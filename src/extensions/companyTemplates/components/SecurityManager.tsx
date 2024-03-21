f95c4a7ba8b4b18badedf6160241a40a7d7dc3cb
Tobias Maestrini <tobias@bee365.ch>
2024-03-22 00:35:30 +0100

    refactor class

--- src/extensions/companyTemplates/components/SecurityManager.tsx
+++ src/extensions/companyTemplates/components/SecurityManager.tsx
@@ -1,7 +1,6 @@
 import { Log } from "@microsoft/sp-core-library";
 
 import * as React from "react"
-
 export type ISecurityManagerProps = {
 
 }
@@ -9,19 +8,17 @@ export type ISecurityManagerProps = {
 const LOG_SOURCE: string = 'Security Manager';
 
 
-export const SecurityManager: React.FunctionComponent<ISecurityManagerProps> = (props: React.PropsWithChildren<ISecurityManagerProps>) => {
+// export const SecurityManager: React.FunctionComponent<ISecurityManagerProps> = (props: React.PropsWithChildren<ISecurityManagerProps>) => {
+export function SecurityManager<ISecurityManagerProps>(props: React.PropsWithChildren<ISecurityManagerProps>): JSX.Element {
   Log.info(LOG_SOURCE, 'SecurityManager loaded');
-  
+
   function evaluate<T>(children: T): T[] {
-      return React.Children.map(children, child => {
-        // TODO: implement rulesets here...
-        // return child && child.key === 'title' ? <h3>{child.props.children}</h3> : null;
-        
-        return child;
-      });
+    return React.Children.map(children, child => {
+      // TODO: implement rulesets here...
+      // return child && child.key === 'title' ? <h3>{child.props.children}</h3> : null;
+      return child;
+    });
   }
 
-  return <>
-    {evaluate(props.children)}
-  </>
+  return <>{evaluate(props.children)}</>
 }
